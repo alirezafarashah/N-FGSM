ce766d7c6df43bf4d511aba200f59330590d3c38
alireza <ali.d.f.2012@gmail.com>
2023-06-13 11:25:34 +0330

    add tiny imagenet for preact_resnet

--- architectures/preact_resnet.py
+++ architectures/preact_resnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class PreActBlock(nn.Module):
     '''Pre-activation version of the BasicBlock.'''
     expansion = 1
@@ -13,9 +14,9 @@ class PreActBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        
-        if stride != 1 or in_planes != self.expansion*planes:
-            downsample_conv = nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+
+        if stride != 1 or in_planes != self.expansion * planes:
+            downsample_conv = nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False)
             self.shortcut = nn.Sequential(downsample_conv)
 
     def forward(self, x):
@@ -38,10 +39,10 @@ class PreActBottleneck(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
 
-        if stride != 1 or in_planes != self.expansion*planes:
-            downsample_conv = nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+        if stride != 1 or in_planes != self.expansion * planes:
+            downsample_conv = nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False)
 
     def forward(self, x):
         out = F.relu(self.bn1(x))
@@ -54,12 +55,12 @@ class PreActBottleneck(nn.Module):
 
 
 class PreActResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, kernel_size=3, stride=1, padding=1):
         super(PreActResNet, self).__init__()
-        
+
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-       
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -68,7 +69,7 @@ class PreActResNet(nn.Module):
         self.linear = nn.Linear(512 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
@@ -89,5 +90,4 @@ class PreActResNet(nn.Module):
 
 
 def PreActResNet18(**kwargs):
-    return PreActResNet(PreActBlock, [2,2,2,2], **kwargs)
-
+    return PreActResNet(PreActBlock, [2, 2, 2, 2], **kwargs)
--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from architectures.preact_resnet import PreActResNet18
 from architectures.wide_resnet import Wide_ResNet
 
 from utils.data_utils import CIFAR10Utils, SVHNUtils, CIFAR100Utils
+from utils.tiny_imagenet import TINYIMAGENETUtils
 from utils.attack_utils import AttackUtils
 
 logger = logging.getLogger(__name__)
@@ -21,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 def get_args():
     parser = argparse.ArgumentParser()
-    
+
     # Architecture settings
     parser.add_argument('--dataset', default='CIFAR10', type=str, help='One of: CIFAR10, CIFAR100 or SVHN')
     parser.add_argument('--architecture', default='PreActResNet18', type=str,
@@ -29,7 +30,7 @@ def get_args():
     parser.add_argument('--wide_resnet_depth', default=28, type=int, help='WideResNet depth')
     parser.add_argument('--wide_resnet_width', default=10, type=int, help='WideResNet width')
     parser.add_argument('--wide_resnet_dropout_rate', default=0.3, type=float, help='WideResNet dropout rate')
-    
+
     # Training schedule settings
     parser.add_argument('--batch-size', default=128, type=int)
     parser.add_argument('--data-dir', default='/path/to/datasets/', type=str)
@@ -39,11 +40,10 @@ def get_args():
     parser.add_argument('--lr-max', default=0.2, type=float)
     parser.add_argument('--weight-decay', default=5e-4, type=float)
     parser.add_argument('--momentum', default=0.9, type=float)
-    
-    
+
     # Adversarial training and evaluation settings
     parser.add_argument('--epsilon', default=8, type=int)
-    parser.add_argument('--epsilon_test', default=None, type=int, 
+    parser.add_argument('--epsilon_test', default=None, type=int,
                         help='''Epsilon to be used at test time (only for final model,
                         if computing loss during training epsilon train is used).
                         If set to None, default, the same args.epsilon will be used for test and train.''')
@@ -63,19 +63,17 @@ def get_args():
     parser.add_argument('--robust_test_size', default=-1, type=int,
                         help='Number of samples to be used for robust testing, Default: -1 will use all samples')
     parser.add_argument('--validation-early-stop', action='store_true',
-        help='Store best epoch via validation')
+                        help='Store best epoch via validation')
 
-    
     # Config paths
     parser.add_argument('--out-dir', default='/path/to/results/',
                         type=str, help='Output directory')
     parser.add_argument('--root-model-dir',
                         default='/path/to/trained/models/',
                         type=str, help='Models directory')
-    
+
     parser.add_argument('--seed', default=0, type=int, help='Random seed')
-    
-    
+
     return parser.parse_args()
 
 
@@ -91,9 +89,12 @@ def main():
         args.lr_max = 0.05
     elif args.dataset.upper() == 'CIFAR100':
         data_utils = CIFAR100Utils()
+    elif args.dataset.upper() == 'TINYIMAGENET':
+        data_utils = TINYIMAGENETUtils()
+        img_size = 64
     else:
         raise ValueError('Unsupported dataset.')
-        
+
     # If args.epsilon_test is None, use the same epsilon than during training.
     if args.epsilon_test is None:
         args.epsilon_test = args.epsilon
@@ -130,14 +131,10 @@ def main():
         valid_size = 0
 
     (train_loader, test_loader, robust_test_loader,
-                valid_loader, train_idx, valid_idx) = data_utils.get_indexed_loaders(args.data_dir,
-                                                                        args.batch_size,
-                                                                        valid_size=valid_size,
-                                                                        robust_test_size=args.robust_test_size)
-
-    # Making sure that data is in the supported format
-    if (data_utils.img_size != (32,32)):
-        raise RuntimeError('Data is not in the supported format input image size (32x32)')
+     valid_loader, train_idx, valid_idx) = data_utils.get_indexed_loaders(args.data_dir,
+                                                                          args.batch_size,
+                                                                          valid_size=valid_size,
+                                                                          robust_test_size=args.robust_test_size)
 
     # Adv training and test settings
     epsilon = (args.epsilon / 255.) / data_utils.std
@@ -145,7 +142,7 @@ def main():
     pgd_alpha = (2 / 255.) / data_utils.std
     if args.clip > 0:
         clip = (args.clip * args.epsilon / 255.) / data_utils.std
-        
+
     # For SVHN, we increase the perturbation radius from 0 to epsilon for first 5 epochs
     if args.dataset.upper() == 'SVHN':
         epsilon_global = epsilon
@@ -154,12 +151,15 @@ def main():
             clip_global = clip
         n_warmup_epochs = 5
         n_warmup_iterations = n_warmup_epochs * len(train_loader)
-        
+
     # Define architecture
-    args.num_classes = data_utils.max_label + 1 # Labels start from 0
+    args.num_classes = data_utils.max_label + 1  # Labels start from 0
     if args.architecture.upper() == 'PREACTRESNET18':
-        model = PreActResNet18(num_classes=args.num_classes).cuda()
-    elif args.architecture.upper() in  'WIDERESNET':
+        if args.dataset.upper() == 'TINYIMAGENET':
+            model = PreActResNet18(kernel_size=7, stride=2, padding=3, num_classes=args.num_classes).cuda()
+        else:
+            model = PreActResNet18(num_classes=args.num_classes).cuda()
+    elif args.architecture.upper() in 'WIDERESNET':
         logger.info(f'Using WideResNet with depth {args.wide_resnet_depth},')
         logger.info(f'width {args.wide_resnet_width} and Dropout rate {args.wide_resnet_dropout_rate}')
         model = Wide_ResNet(args.wide_resnet_depth,
@@ -171,17 +171,18 @@ def main():
         raise ValueError('Unknown architecture.')
 
     model.train()
-    
+
     opt = torch.optim.SGD(model.parameters(), lr=args.lr_max, momentum=args.momentum, weight_decay=args.weight_decay)
 
     lr_steps = args.epochs * len(train_loader)
     if args.lr_schedule == 'cyclic':
         if args.dataset.upper() == 'SVHN':
             scheduler = torch.optim.lr_scheduler.CyclicLR(opt, base_lr=args.lr_min, max_lr=args.lr_max,
-                step_size_up=lr_steps * 2 / 5, step_size_down=lr_steps * 3 / 5)
+                                                          step_size_up=lr_steps * 2 / 5,
+                                                          step_size_down=lr_steps * 3 / 5)
         else:
             scheduler = torch.optim.lr_scheduler.CyclicLR(opt, base_lr=args.lr_min, max_lr=args.lr_max,
-                step_size_up=lr_steps / 2, step_size_down=lr_steps / 2)
+                                                          step_size_up=lr_steps / 2, step_size_down=lr_steps / 2)
     elif args.lr_schedule == 'multistep':
         scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=[lr_steps / 2, lr_steps * 3 / 4], gamma=0.1)
 
@@ -192,11 +193,12 @@ def main():
         robust_val_acc_hist = []
 
     logger.info('Epoch \t Seconds \t LR \t \t Train Loss \t Train Acc')
+    print('Epoch \t Seconds \t LR \t \t Train Loss \t Train Acc')
     iter_count = 0
 
     max_iter = len(train_loader) * args.epochs
     counter_iter = 0
-    
+
     for epoch in range(args.epochs):
         start_epoch_time = time.time()
         train_loss = 0
@@ -209,10 +211,10 @@ def main():
                 alpha = alpha_global * min(iter_count / n_warmup_iterations, 1)
                 if args.clip > 0:
                     clip = clip_global * min(iter_count / n_warmup_iterations, 1)
-                
+
             batch_size_ = X.shape[0]
             X, y = X.cuda(), y.cuda()
-            
+
             # Initialize random step
             eta = torch.zeros_like(X).cuda()
             if args.unif > 0:
@@ -233,7 +235,7 @@ def main():
                 delta = attack_utils.clamp(delta, -clip, clip)
 
             delta = delta.detach()
-            
+
             output = model(X + delta)
 
             # Training step
@@ -241,7 +243,7 @@ def main():
             opt.zero_grad()
             loss.backward()
             opt.step()
-            
+
             train_loss += loss.item() * y.size(0)
             train_acc += (output.max(1)[1] == y).sum().item()
             train_n += y.size(0)
@@ -252,6 +254,8 @@ def main():
         if args.validation_early_stop:
             pgd_loss, pgd_acc = attack_utils.evaluate_pgd(valid_loader, model, 10, 1, epsilon=args.epsilon)
             test_loss, test_acc = attack_utils.evaluate_standard(valid_loader, model)
+            print("validation pgd10 test-acc, pgd-acc")
+            print(test_acc, pgd_acc)
             # After evaluating the model, set model to train mode again
             model.train()
             if pgd_acc >= best_pgd_val_acc:
@@ -263,7 +267,8 @@ def main():
         epoch_time = time.time()
         lr = scheduler.get_last_lr()[0]
         logger.info('%d \t %.1f \t \t %.4f \t %.4f \t %.4f',
-            epoch, epoch_time - start_epoch_time, lr, train_loss/train_n, train_acc/train_n)
+                    epoch, epoch_time - start_epoch_time, lr, train_loss / train_n, train_acc / train_n)
+        print(epoch, epoch_time - start_epoch_time, lr, train_loss / train_n, train_acc / train_n)
 
     train_time = time.time()
     if args.validation_early_stop:
@@ -272,27 +277,26 @@ def main():
     final_state_dict = model.state_dict()
     torch.save(final_state_dict, os.path.join(model_path, 'model.pth'))
 
-    logger.info('Total train time: %.4f minutes', (train_time - start_train_time)/60)
+    logger.info('Total train time: %.4f minutes', (train_time - start_train_time) / 60)
 
     if args.validation_early_stop:
         np.save(os.path.join(model_path, 'val_acc_hist.npy'), val_acc_hist)
         np.save(os.path.join(model_path, 'robust_val_acc.npy'), robust_val_acc_hist)
 
-
     if args.robust_test_size != 0:
         # Evaluation final model
         print('Training finished, starting evaluation')
-        args.num_classes = data_utils.max_label + 1 # Labels start from 0
+        args.num_classes = data_utils.max_label + 1  # Labels start from 0
         if args.architecture.upper() == 'PREACTRESNET18':
             model_test = PreActResNet18(num_classes=args.num_classes).cuda()
-        elif args.architecture.upper() in  'WIDERESNET':
+        elif args.architecture.upper() in 'WIDERESNET':
             logger.info(f'Using WideResNet with depth {args.wide_resnet_depth},')
             logger.info(f'width {args.wide_resnet_width} and Dropout rate {args.wide_resnet_dropout_rate}')
             model_test = Wide_ResNet(args.wide_resnet_depth,
-                                args.wide_resnet_width,
-                                args.wide_resnet_dropout_rate,
-                                num_classes=args.num_classes).cuda()
-            
+                                     args.wide_resnet_width,
+                                     args.wide_resnet_dropout_rate,
+                                     num_classes=args.num_classes).cuda()
+
         model_test.load_state_dict(final_state_dict)
         model_test.float()
         model_test.eval()
@@ -304,32 +308,6 @@ def main():
         logger.info('%.4f \t \t %.4f \t %.4f \t %.4f', test_loss, test_acc, pgd_loss, pgd_acc)
         print('Finished evaluating final model')
 
-        if args.validation_early_stop:
-            # Evaluation best model
-            print('Evaluating best model')
-            args.num_classes = data_utils.max_label + 1 # Labels start from 0
-            print('Training finished, starting evaluation')
-        
-            args.num_classes = data_utils.max_label + 1 # Labels start from 0
-            if args.architecture.upper() == 'PREACTRESNET18':
-                best_model = PreActResNet18(num_classes=args.num_classes).cuda()
-            elif args.architecture.upper() in  'WIDERESNET':
-                logger.info(f'Using WideResNet with depth {args.wide_resnet_depth},')
-                logger.info(f'width {args.wide_resnet_width} and Dropout rate {args.wide_resnet_dropout_rate}')
-                best_model = Wide_ResNet(args.wide_resnet_depth,
-                                    args.wide_resnet_width,
-                                    args.wide_resnet_dropout_rate,
-                                    num_classes=args.num_classes).cuda()
-
-            best_model.load_state_dict(best_state_dict)
-            best_model.float()
-            best_model.eval()
-            
-            best_pgd_loss, best_pgd_acc = attack_utils.evaluate_pgd(robust_test_loader, best_model, 50, 10, epsilon=args.epsilon_test)
-            best_test_loss, best_test_acc = attack_utils.evaluate_standard(test_loader, best_model)
-
-            logger.info('Best test Loss \t Best test Acc \t Best PGD Loss \t Best PGD Acc')
-            logger.info('%.4f \t \t %.4f \t %.4f \t %.4f', best_test_loss, best_test_acc, best_pgd_loss, best_pgd_acc)
 
 if __name__ == "__main__":
     main()
